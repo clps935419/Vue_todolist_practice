496bac2a5d0075fb6209bde535fa9cf9966a0d09
clps935419 <clps935419@gmail.com>
2018-06-08 14:48:06 +0800

    ㄋ
    asdas

--- Vue_todolist/js/all.js
+++ Vue_todolist/js/all.js
@@ -1,26 +1,36 @@
 var app=new Vue({
     el:'#app',
     data:{
+        showlist:true,
+        showbox:true,
+        show:false,
+        changeColor:false,
         newtodoTitle:'',
         newTodoTime1:'',
         newTodoTime2:'',
         comment:'',
+        visability:'all',
         todo:[
-            {
-                id:'',
-                title:'預設',
-                time:'預設',
-                comment:'預設',
-                complet:''
-            }
-        ]
+            // {
+            //     id:'',
+            //     title:'',
+            //     time:'',
+            //     comment:'',
+            //     complet:''
+            // }
+        ],
+        
     },
     methods:{
+        
         add:function(){
-            var new_title=this.newtodoTitle;
-            var Timevalue=this.newTodoTime1+this.newTodoTime2;
+            var new_title=this.newtodoTitle.trim();
+            var Timevalue=this.newTodoTime1+this.newTodoTime2.trim();
             var time=Math.floor(Date.now());
             // console.log(value+time);
+            if(!new_title &&!Timevalue){
+                return alert('請輸入資料');
+            }
             this.todo.push({
                 id:time,
                 title:new_title,
@@ -31,6 +41,9 @@ var app=new Vue({
             this.newTodoTime1='';
             this.newTodoTime2='';
             this.comment='';
+            
+            this.showlist=!this.showlist;
+            
         },
         remove:function(key){
             this.todo.splice(key,1);
@@ -41,6 +54,31 @@ var app=new Vue({
             this.newTodoTime1='';
             this.newTodoTime2='';
             this.comment='';
+            this.show=!this.show;
+        },
+       
+    },
+    computed:{
+        filtertodo:function(){
+            if(this.visability=='all'){
+                return this.todo;
+            }else if(this.visability=='act'){
+                var newtodo=[];
+                this.todo.forEach(function(item){
+                    if(!item.complete){
+                        newtodo.push(item);
+                    }
+                })
+                return newtodo;
+            }else if(this.visability=='complte'){
+                var newtodo=[];
+                this.todo.forEach(function(item){
+                    if(item.complete){
+                        newtodo.push(item);
+                    }
+                })
+                return newtodo;
+            }
         }
     }
 })
